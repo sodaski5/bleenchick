35be87317b22ac9229a41ef13e117907dd1c9799
sodaski5 <sodaski5@gmail.com>
2022-02-19 03:15:04 +0200

    Trial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bleenchik.wsgi
--- bleenchik/settings.py
+++ bleenchik/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c7-*pn!x*vhn(gi3^w84rqn)bijze0qvks9bcy^84r29lxy*2)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bleenchik.herokuapp.com']
 
 
 # Application definition
@@ -126,3 +126,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
